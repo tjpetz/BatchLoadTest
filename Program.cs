364bbdeb11729dd3f078052bbc9c5dc699004ff6
Tom Petz <tom@tjpetz.com>
2023-01-18 19:13:29 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BatchLoadTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
